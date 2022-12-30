0e93a84e3c4328d3e821011fd7f22865a0f64f76
Sotatek-DucNguyen10 <duc.nguyen10@sotatek.com>
2022-12-30 13:26:34 +0700

    reactive modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { HealthCheckModule } from './healthcheck/healthcheck.module';
-// import { PrismaModule } from './prisma/prisma.module';
-// import { RecordModule } from './record/record.module';
-// import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RecordModule } from './record/record.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    // AuthModule,
-    // UserModule,
-    // RecordModule,
-    // PrismaModule,
+    AuthModule,
+    UserModule,
+    RecordModule,
+    PrismaModule,
     HealthCheckModule,
   ],
 })
